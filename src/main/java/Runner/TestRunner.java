6eeef577563361967db4d7cdcc43adae4c0259d7
Jignesh45 <jignesh.wala1994@gmail.com>
2020-04-08 19:45:24 +0530

    test Runner class updated with commit

--- src/main/java/Runner/TestRunner.java
+++ src/main/java/Runner/TestRunner.java
@@ -7,11 +7,12 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features ="src/main/java/Features",
-        glue = {"StepDefinitions"},
-        format = {"pretty","html:test-output","json:json_output/cucumber.json","junit:junit_xml/cucumber_xml"},
-        monochrome = true,
-        dryRun = false,
-        strict = true
+        glue={"StepDefinitions"}, //the path of the step definition files
+        format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
+        monochrome = true, //display the console output in a proper readable format
+        strict = true, //it will check if any step is not defined in step definition file
+        dryRun = false //to check the mapping is proper between feature file and step def file
+        //tags = {"~@SmokeTest" , "~@RegressionTest", "~@End2End"}
 )
 public class TestRunner {
 
